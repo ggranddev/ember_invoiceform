f5bdb8aa752dbed5d0d52c47866b5b3969475be4
ggranddev <blue.webking960112@gmail.com>
2019-05-12 22:51:04 -1000

    add component lifecycle

--- app/components/add-form.js
+++ app/components/add-form.js
@@ -2,9 +2,32 @@ import Component from '@ember/component';
 
 export default Component.extend({
 	store: Ember.inject.service(),
+	init() {
+		this._super(...arguments);
+		this.set('error', []);
+		console.log("add component init");
+	},
+
+	didReceiveAttrs() {
+		console.log("did receive attr");
+	},
+
+	willRender() {
+		console.log("will render");
+	},
+
+	didRender() {
+		console.log("did render");
+	},
+
+	didInsertElement() {
+		console.log("insert element");
+	},
+
 	actions: {
 		addInvoice() {
 			console.log("addd invoices", this.get('invoices'));
+			
 		}
 	}
 });
